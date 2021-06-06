71e4d2bbf71e8c6a40f7074ec2ffd4f9926f600b
lemon_lyue <lemon_lyue@163.com>
2021-06-06 15:53:48 +0800

    docs: ✏️ amqp retry config

--- publish/amqp_retry.php
+++ publish/amqp_retry.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
-    'retry_count' => (int)env('AMQP_RETRY_COUNT', 3),
-    'retry_time_interval' => (int)env('AMQP_RETRY_TIME_INTERVAL', 1),
-    'task_table' => env('AMQP_TASK_TABLE', 'task'),
+    'retry_count' => (int)env('AMQP_RETRY_COUNT', 3),// 重试次数
+    'retry_time_interval' => (int)env('AMQP_RETRY_TIME_INTERVAL', 1),// 重试间隔时间
+    'task_table' => env('AMQP_TASK_TABLE', 'task'),// 记录消费消息表名
 ];
